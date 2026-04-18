b67d0092fffdfc8bc3692c6dc99c34d3ea6a5407
jpcpsgodoy <jpcpsgodoy@gmail.com>
2026-04-18 13:05:17 -0300

    feat: inicializa conexão com o supabase no ponto de entrada

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'views/formulario_denuncia_screen.dart';
 
-void main() => runApp(const ReporteroApp());
+// F1-01: Configuração do Supabase e estrutura básica do app
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Configuração do Supabase
+  await Supabase.initialize(
+    url: 'https://fxkelgxlfddybvtmpzye.supabase.co',
+    anonKey: 'sb_publishable_0trdTilYVLii8p2kmnGSrA_MgwJY5ki',
+  );
+  // Inicia o app
+  runApp(const ReporteroApp());
+}
 
 class ReporteroApp extends StatelessWidget {
   const ReporteroApp({super.key});
@@ -11,9 +22,12 @@ class ReporteroApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Reportero Unicamp',
-      theme: ThemeData(useMaterial3: true, colorSchemeSeed: const Color(0xFF37474F)),
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: const Color(0xFF37474F),
+      ),
       // Rotas nomeadas facilitam o trabalho do Gabriel (Feed) e Gilberth (Navegação)
-      initialRoute: '/nova', 
+      initialRoute: '/nova',
       routes: {
         '/nova': (context) => const FormularioDenunciaScreen(),
         // '/feed': (context) => const FeedScreen(), // Gabriel adicionará depois
